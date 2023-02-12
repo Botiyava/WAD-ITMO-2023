ac57550e06cafad38f0e066f6b87defba6f4df9a
Leonid Shalaev <botiyava.prog@mail.ru>
2023-02-12 16:51:22 +0300

    added fist html page

--- homework1/main.py
+++ homework1/main.py
@@ -1,8 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!<p>"
+def get_index():
+    name = "Leo"
+    deadline = datetime.datetime.strptime('2023/02/17', "%Y/%m/%d")
+    time_before_deadline = deadline - datetime.datetime.utcnow().replace(microsecond=0)
+#   a = {"utc": "1"}
+#   return render_template("index.html", **a )
+    return render_template("index.html",
+                           name=name,
+                           time_before_deadline=time_before_deadline)
