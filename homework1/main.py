ce5171db238705a9eaff3111d16d0e38a2e5a4f6
Leonid Shalaev <botiyava.prog@mail.ru>
2023-02-15 20:42:51 +0300

    added profile page, added reroute to profile page from default path

--- homework1/main.py
+++ homework1/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 import datetime
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def get_index():
+@app.route('/')
+def index():
+    return redirect(url_for('get_profile'), 301)
+
+
+@app.route('/profile')
+def get_profile():
     name = "Leo"
     deadline = datetime.datetime.strptime('2023/02/17', "%Y/%m/%d")
     time_before_deadline = deadline - datetime.datetime.utcnow().replace(microsecond=0)
