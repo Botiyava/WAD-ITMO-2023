92d3950f8138416007ded23876d7f955f3022789
Leonid Shalaev <botiyava.prog@mail.ru>
2023-02-16 15:20:21 +0300

    added footer variables

--- homework1/main.py
+++ homework1/main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, url_for, redirect
 import datetime
+import yaml
+
+
+def get_footer_vars():
+    with open('project_info/info.yaml', 'r') as file:
+        footer_vars = yaml.safe_load(file)
+    return footer_vars
 
 
 app = Flask(__name__)
@@ -12,6 +19,7 @@ def index():
 
 @app.route('/profile')
 def get_profile():
+    vars = get_footer_vars()
     name = "Leo"
     deadline = datetime.datetime.strptime('2023/02/17', "%Y/%m/%d")
     time_before_deadline = deadline - datetime.datetime.utcnow().replace(microsecond=0)
@@ -19,4 +27,6 @@ def get_profile():
 #   return render_template("index.html", **a )
     return render_template("index.html",
                            name=name,
-                           time_before_deadline=time_before_deadline)
+                           time_before_deadline=time_before_deadline,
+                           version = vars['version'],
+                           owner = vars['owner'])
